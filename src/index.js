659a91369eb63d497fb5a6afb1e8667f75c93d8a
batuhan.duras <batugane@gmail.com>
2024-07-17 10:27:20 +0300

    feat: update console log message

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-'use strict';
+'use strict'
 
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 app.get('/hello', (req, res) => res.send('Hello World'))
 
 const port = 3000
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}! \n This is Version 2 of the app`))
